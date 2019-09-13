94a4e9acee740a493b922928157c049185b1ede4
cm1900 <cmalebe139@gmail.com>
2019-09-13 08:40:05 +0200

    Refined popup messages

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ data = pandas.read_csv("volcanoes.txt")
 lat = list(data['LAT'])
 lon = list(data['LON'])
 name = list(data['NAME'])
+loc = list(data['LOCATION'])
+elev = list(data['ELEV'])
 
 map = folium.Map(location =[38.58, -99.09], zoom_start = 4, tiles = "Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, nm in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location =[lt, ln], popup =nm, icon =folium.Icon(color='red')))
+for lt, ln, nm, lc, elv in zip(lat, lon, name, loc, elev):
+    msg = "Name: {}\nLocation: {}\nElevation: {} ".format(nm, lc, elv)
+    fg.add_child(folium.Marker(location =[lt, ln], popup =msg, icon =folium.Icon(color='red')))
 
 map.add_child(fg)
 map.save("map1.html")
