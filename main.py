50c16f1dd65465815189fe18b0da908af146a7df
cm1900 <cmalebe139@gmail.com>
2019-09-13 08:18:42 +0200

    Retrieve volcanoe info and add to feature group

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import folium
-map = folium.Map(location =[-10, 28], zoom_start = 3, tiles = "Stamen Terrain")
+import pandas
+
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data['LAT'])
+lon = list(data['LON'])
+name = list(data['NAME'])
+
+map = folium.Map(location =[38.58, -99.09], zoom_start = 4, tiles = "Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location =[-3, 28], popup ="Danger Zone", icon =folium.Icon(color='red')))
+
+for lt, ln, nm in zip(lat, lon, name):
+    fg.add_child(folium.Marker(location =[lt, ln], popup =nm, icon =folium.Icon(color='red')))
+
 map.add_child(fg)
 map.save("map1.html")
