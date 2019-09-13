593ced49a27eeabd6963606eb7cda8d46b99c20b
cm1900 <cmalebe139@gmail.com>
2019-09-13 07:26:59 +0200

    Create folium feature group and add to map

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import folium
 map = folium.Map(location =[-10, 28], zoom_start = 3, tiles = "Stamen Terrain")
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location =[-3, 28], popup ="Danger Zone", icon =folium.Icon(color='red')))
+map.add_child(fg)
 map.save("map1.html")
